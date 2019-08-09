d635a83a55bca6ae75837981354b4ab2e7b68647
Arul mozhi <arulmozhi@Aruls-MacBook-Pro.local>
2019-08-09 20:51:52 +0200

    create react app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
+import Navbar from './components/left-navbar'
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div class="container">
+          <div class="row">
+          <div Class="sidebar col-4 ">
+              <Navbar/>
+
+          </div>
+
+          <div class="col-8">
+
+
+          </div>
+      </div>
+
+      </div>
+
+
   );
 }
 
---
+++ src/components/left-navbar.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+function Navbar(){
+
+    return(
+        <ul class="list-group">
+            <li class="list-group-item">Categories</li>
+            <li class="list-group-item">Camera</li>
+            <li class="list-group-item">Photos</li>
+            <li class="list-group-item">Setting</li>
+
+        </ul>
+
+
+    )
+
+
+}
+export default Navbar;
