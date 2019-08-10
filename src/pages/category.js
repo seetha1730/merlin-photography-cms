10ca84c856cdfa01a963ee00290996d714ef1b44
Natarajan <arul.natarajan@rabobank.nl>
2019-08-10 22:43:32 +0200

    update category page

--- src/pages/category.js
+++ src/pages/category.js
@@ -2,9 +2,47 @@ import React from 'react';
 
 function Category() {
 
-return(
-    <h1>Categories</h1>
-);
+    return (
+        <div>
+            <h1>View Categories</h1>
+            <table className="table table-striped">
+                <thead>
+                    <tr>
+                        <th scope="col">#</th>
+                        <th scope="col">Name</th>
+                        <th scope="col">setting</th>
+                        
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <th scope="row">1</th>
+                        <td>Macro</td>
+                        <td><p className="underline">Edit</p>/<p className="underline">delete</p></td>
+                       
+                    </tr>
+                    <tr>
+                        <th scope="row">2</th>
+                        <td>Portsit</td>
+                        <td><p className="underline">Edit</p>/<p className="underline">delete</p></td>
+                    </tr>
+                    <tr>
+                        <th scope="row">3</th>
+                        <td>Insects</td>
+                        <td><p className="underline">Edit</p>/<p className="underline">delete</p></td>
+                    </tr>
+                </tbody>
+            </table>
+            <form>
+                <div className="form-group">
+                    <h3>Add Category</h3>
+                    <input type="text" className="form-control" id="add-category" placeholder="add categories" />
+                </div>
+                <button type="button" className="btn btn-primary ">Add</button>
+
+            </form>
+        </div>
+    );
 
 }
 
