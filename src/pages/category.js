bb2a74612195c8d7a50d233eb106d89c6823e0d3
seetha lakshmi <seethalakshmi@seethas-MacBook-Pro.local>
2019-08-11 22:03:01 +0200

    changed functional component into react component

--- src/pages/category.js
+++ src/pages/category.js
@@ -1,25 +1,38 @@
 import React from 'react';
 
-function Category() {
+class Category extends React.Component {
 
-    return (
-        <div>
-            <h2>View Categories</h2>
-            <table className="table table-striped">
-                <thead>
+
+    state = {
+        category:'',
+    }
+
+    addCategory(){
+        //http post -> api send
+        console.log(this.state.category)
+
+        //http get => api new values
+    }
+
+    render() {
+        return (
+            <div>
+                <h2>View Categories</h2>
+                <table className="table table-striped">
+                    <thead>
                     <tr>
                         <th scope="col">#</th>
                         <th scope="col">Name</th>
                         <th scope="col">setting</th>
-                        
+
                     </tr>
-                </thead>
-                <tbody>
+                    </thead>
+                    <tbody>
                     <tr>
                         <th scope="row">1</th>
                         <td>Macro</td>
                         <td><p className="underline">Edit</p>/<p className="underline">delete</p></td>
-                       
+
                     </tr>
                     <tr>
                         <th scope="row">2</th>
@@ -31,18 +44,21 @@ function Category() {
                         <td>Insects</td>
                         <td><p className="underline">Edit</p>/<p className="underline">delete</p></td>
                     </tr>
-                </tbody>
-            </table>
-            <form>
-                <div className="form-group">
-                    <h3>Add Category</h3>
-                    <input type="text" className="form-control" id="add-category" placeholder="add categories" />
-                </div>
-                <button type="button" className="btn btn-primary ">Add</button>
+                    </tbody>
+                </table>
+                <form>
+                    <div className="form-group">
+                        <h3>Add Category</h3>
+                        <input type="text" className="form-control" id="add-category" placeholder="add categories" onChange={(e) => this.setState({category: e.target.value})}/>
+                    </div>
+                    <button type="button" className="btn btn-primary" onClick={() => this.addCategory()}>Add</button>
 
-            </form>
-        </div>
-    );
+                </form>
+            </div>
+        );
+    }
+
+
 
 }
 
